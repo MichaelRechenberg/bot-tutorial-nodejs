b25b709ba1e626380c3d3079d3ced5d52f2a4afa
MichaelRechenberg <mikerechenberg@owc.net>
2017-05-20 18:59:25 -0500

    ...

--- bot.js
+++ bot.js
@@ -21,8 +21,12 @@ function respond() {
   if(request.text && navySealRegex.test(request.text)){
     //Navy Seal Copy Pasta
     this.res.writeHead(200);
-    var message = 'What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.';
-    postMessage(message);
+    var part1 = 'What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words.';
+    var part2 = 'You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit.';
+    var part3 = 'If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.';
+    postMessage(part1);
+    postMessage(part2);
+    postMessage(part3);
   }
   else if(request.text && coolGuyRegex.test(request.text)) {
     //Cool Guy Face
@@ -32,7 +36,9 @@ function respond() {
   } 
   else if(request.text && rollRegex.test(request.text)){
 
-    var dice_size = parseInt(rollRegex.exec(rollRegex)[1]);
+    //extract the dice size from the command
+    var dice_size = parseInt(rollRegex.exec(request.text)[1]);
+    //can't roll a 0-sided die :(
     if(dice_size > 0){
       var result = Math.floor(Math.random() * (dice_size - 1)) + 1;
       this.res.writeHead(200);
